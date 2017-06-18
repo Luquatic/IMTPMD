e4fed0317465b8863411d1d09060e0842352b9f3
Luquatic <jesseyfransen@live.nl>
2017-06-18 21:45:48 +0200

    created Compose, fab works now.

--- app/src/main/java/imtpmd/jb_app_imtpmd/course_activity.java
+++ app/src/main/java/imtpmd/jb_app_imtpmd/course_activity.java
@@ -1,11 +1,15 @@
 package imtpmd.jb_app_imtpmd;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class course_activity extends AppCompatActivity {
     private String studiejaar;
     private String student_naam;
+    FloatingActionButton fab_add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,15 @@ public class course_activity extends AppCompatActivity {
         studiejaar = getIntent().getStringExtra("Jaar");
         student_naam = getIntent().getStringExtra("student");
         setTitle(studiejaar + " van " + student_naam);
+
+        // floating action button click event
+        fab_add = (FloatingActionButton)findViewById(R.id.fab_add);
+        fab_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(course_activity.this, Compose.class));
+            }
+        });
 
 
     }
