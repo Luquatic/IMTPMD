1b6cf616c464d2616415f8a42c4ea93fbc319b47
Luquatic <jesseyfransen@live.nl>
2017-06-19 14:13:00 +0200

    added menu top right

--- app/src/main/java/imtpmd/jb_app_imtpmd/MainActivity.java
+++ app/src/main/java/imtpmd/jb_app_imtpmd/MainActivity.java
@@ -10,6 +10,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -188,5 +190,26 @@ public class MainActivity extends AppCompatActivity {
         });
 
         namebuilder.show();
+    }
+
+    // menu button top right
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    // menu event handler
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(MainActivity.this, Settings.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
