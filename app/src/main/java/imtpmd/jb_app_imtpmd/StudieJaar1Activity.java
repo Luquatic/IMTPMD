744ed8b7df094b75c8a1e48f4f38df1cfc3dc84f
Luquatic <jesseyfransen@live.nl>
2017-06-21 11:53:47 +0200

    list changes

--- app/src/main/java/imtpmd/jb_app_imtpmd/StudieJaar1Activity.java
+++ app/src/main/java/imtpmd/jb_app_imtpmd/StudieJaar1Activity.java
@@ -1,15 +1,18 @@
-package imtpmd.jb_app_imtpmd;
+package git;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,8 +24,13 @@ import java.util.List;
 public class StudieJaar1Activity extends AppCompatActivity {
 
     // declaring variables private for class
-    private ListView vakken_list_view_p1, vakken_list_view_p2, vakken_list_view_p3, vakken_list_view_p4;
+    private ListView vakken_list_view;
     private String student_naam;
+    private String alle_periodes[];
+    private String periode_1[];
+    private String periode_2[];
+    private String periode_3[];
+    private String periode_4[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +40,14 @@ public class StudieJaar1Activity extends AppCompatActivity {
         student_naam = getIntent().getStringExtra("student");
         setTitle("Studiejaar 1 van " + student_naam);
 
-        String[] periode_1 = {
+        final String[] periode_1 = {
                 "IARCH",
                 "IIBPM",
                 "IHBO",
                 "IOPR1"
         };
 
-        String[] periode_2 = {
+        final String[] periode_2 = {
                 "IWDR",
                 "IRDB",
                 "IIBUI",
@@ -48,7 +56,7 @@ public class StudieJaar1Activity extends AppCompatActivity {
                 "IPOMEDT"
         };
 
-        String[] periode_3 = {
+        final String[] periode_3 = {
                 "IMUML",
                 "IOPR2",
                 "IFIT",
@@ -56,44 +64,68 @@ public class StudieJaar1Activity extends AppCompatActivity {
                 "IPOSE"
         };
 
-        String[] periode_4 = {
+        final String[] periode_4 = {
                 "IPROV",
                 "ICOMMP",
                 "ISLP"
         };
 
+        final String[] alle_periodes = {
+                "IARCH",
+                "IIBPM",
+                "IHBO",
+                "IOPR1",
+                "IWDR",
+                "IRDB",
+                "IIBUI",
+                "INET",
+                "IPRODAM",
+                "IPOMEDT",
+                "IMUML",
+                "IOPR2",
+                "IFIT",
+                "IPOFIT",
+                "IPOSE",
+                "IPROV",
+                "ICOMMP",
+                "ISLP"
+        };
+
+        // spinner top right
+        Spinner spinner = (Spinner) findViewById(R.id.periode_spinner);
+        // create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.periodes_array, android.R.layout.simple_spinner_item);
+        // specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                if (item.equals("Alle Periodes")) {
+                    vakken_list_view.setAdapter(new ArrayAdapter<String>(StudieJaar1Activity.this, android.R.layout.simple_list_item_1, alle_periodes));
+                } else if (item.equals("Periode 1")) {
+                    vakken_list_view.setAdapter(new ArrayAdapter<String>(StudieJaar1Activity.this, android.R.layout.simple_list_item_1, periode_1));
+                } else if (item.equals("Periode 2")) {
+                    vakken_list_view.setAdapter(new ArrayAdapter<String>(StudieJaar1Activity.this, android.R.layout.simple_list_item_1, periode_2));
+                } else if (item.equals("Periode 3")) {
+                    vakken_list_view.setAdapter(new ArrayAdapter<String>(StudieJaar1Activity.this, android.R.layout.simple_list_item_1, periode_3));
+                } else if (item.equals("Periode 4")) {
+                    vakken_list_view.setAdapter(new ArrayAdapter<String>(StudieJaar1Activity.this, android.R.layout.simple_list_item_1, periode_4));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         // listview for course years
-        vakken_list_view_p1 = (ListView) findViewById(R.id.vakken_list_p1);
-        vakken_list_view_p2 = (ListView) findViewById(R.id.vakken_list_p2);
-        vakken_list_view_p3 = (ListView) findViewById(R.id.vakken_list_p3);
-        vakken_list_view_p4 = (ListView) findViewById(R.id.vakken_list_p4);
+        vakken_list_view = (ListView) findViewById(R.id.vakken_list);
 
         // arrayadapter for jaar_list_view listview
-        vakken_list_view_p1.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, periode_1));
-        vakken_list_view_p2.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, periode_2));
-        vakken_list_view_p3.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, periode_3));
-        vakken_list_view_p4.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, periode_4));
-    }
-
-    public static class ListUtils {
-        public static void setDynamicHeight(ListView mListView) {
-            ListAdapter mListAdapter = mListView.getAdapter();
-            if (mListAdapter == null) {
-                // when adapter is null
-                return;
-            }
-            int height = 0;
-            int desiredWidth = View.MeasureSpec.makeMeasureSpec(mListView.getWidth(), View.MeasureSpec.UNSPECIFIED);
-            for (int i = 0; i < mListAdapter.getCount(); i++) {
-                View listItem = mListAdapter.getView(i, null, mListView);
-                listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
-                height += listItem.getMeasuredHeight();
-            }
-            ViewGroup.LayoutParams params = mListView.getLayoutParams();
-            params.height = height + (mListView.getDividerHeight() * (mListAdapter.getCount() - 1));
-            mListView.setLayoutParams(params);
-            mListView.requestLayout();
-        }
+        vakken_list_view.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alle_periodes));
     }
 
     // menu button top right
